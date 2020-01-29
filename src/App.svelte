ab51651351ffc82fa4d391ebc6bad85141f8573d
WardToulet <ward@toulet.net>
2020-01-29 14:39:08 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Map from './Map.svelte';
+	import YearSlider from './YearSlider.svelte';
+
+	let namesByYearByPlace = getJSON('byYearByPlace');
+	let placeNameById = getJSON('gemeenten');
+
+	async function getJSON(name) {
+		let res = await fetch(`./data/${name}.json`);
+		let obj = await res.json();
+
+		console.log(obj);
+
+		if(!res.ok)
+			return new Error(obj);
+
+		return obj;
+	}
+
+	function test(e) {
+		console.log(e.detail);
+	}
+
+	function test2(e) {
+		console.log(e.detail);
+	}
+</script>
+
+<main>
+	<Map on:test={test2}//>
+	<YearSlider min="2009" max="2019" default="2012" on:year={test}/>
+</main>
---
+++ src/YearSlider.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher(); 
+	export let min;
+	export let max;
+	export let def = Integer(min + ((max - min) / 2));
+
+	function changeYear(e) {
+		dispatch('year', {
+			year: e.target.value
+		});	
+	}
+</script>
+
+<input id="year_inp" 
+       type="range" 
+       name="year_inp"
+       min={min}
+       max={max}
+       value={def}
+       on:input={changeYear}
+>
