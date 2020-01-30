0bf2d7246d01e74932bfe190b5d6a8615b3b59e4
WardToulet <ward@toulet.net>
2020-01-30 10:10:21 +0000

    fixed graph.js import

--- src/Graphs.svelte
+++ src/Graphs.svelte
@@ -1,35 +1,38 @@
+<svelte:head>
+	<script src="chart.js" on:load={ chart }></script>
+</svelte:head>
+
 <script>
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher(); 
-    
-    export let location;
-    export let labels;
-    export let values;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher(); 
+	
+	export let location;
+	export let labels; export let values; 
+	let ctx = document.getElementById('myChart');
 
-    var ctx = document.getElementById('myChart');
+	function chart() {
+		let chart = new Chart(ctx, {
+			// The type of chart we want to create
+			type: 'bar',
 
-    let chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
+			// The data for our dataset
+			data: {
+				labels: labels,
+				datasets: [{
+					label: location,
+					backgroundColor: function(context) {
+						let index = context.dataIndex;
+						let value = context.dataset.data[index];
+						return index % 2 ? 'black' : 'white';
+				  },
+					borderColor: 'black',
+					data: values
+				}]
+			},
 
-        // The data for our dataset
-        data: {
-            labels: labels,
-            datasets: [{
-                label: location,
-                backgroundColor: function(context) {
-                    var index = context.dataIndex;
-                    var value = context.dataset.data[index];
-                    return index % 2 ? 'black' : 'white';
-              },
-                borderColor: 'black',
-                data: values
-            }]
-        },
-
-        // Configuration options go here
-        options: {}
-    });
+			// Configuration options go here
+			options: {}
+		});
+	}
 </script>
-
 <canvas id="myChart"></canvas>
